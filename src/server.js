5646a0acc4426d0e1e033de5a14430154b6712dd
EJGDLG <edwinjosegabriel@gmail.com>
2025-05-08 02:18:59 -0600

    server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,32 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const rutas = require('./routes/index');
-
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+const rutasUsuario = require('./routes/index');
+const rutasRestaurante = require('./routes/restaurantes');
+const menuRoutes   = require('./routes/menu');
+const ordenRoutes  = require('./routes/orden');
+const resenaRoutes = require('./routes/resena');
+const rutasRest = require('./routes/restaurantes');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use('/api', rutasUsuario);
+app.use('/api/restaurantes', rutasRestaurante);  // <- Esta línea importa
+app.use('/api/menus',   menuRoutes);
+app.use('/api/ordenes', ordenRoutes);
+app.use('/api/resenas', resenaRoutes);
+app.use('/api/restaurantes', rutasRest);
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('Error de conexión:', err));
-
-app.use('/api', rutas);
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB conectado'))
+.catch(err => console.error('Error de conexión:', err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
