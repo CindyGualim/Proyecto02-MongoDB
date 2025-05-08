e361db9a9355b85b3766613541e3a72fe7ce8a94
EJGDLG <edwinjosegabriel@gmail.com>
2025-05-08 00:04:30 -0600

    base0.2

--- src/server.js
+++ src/server.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const rutas = require('./routes/index');
+
 require('dotenv').config();
 
-const rutasUsuario = require('./routes/index');
-const rutasRestaurante = require('./routes/restaurant.routes');
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', rutasUsuario);
-app.use('/api/restaurantes', rutasRestaurante);  // <- Esta línea importa
-
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB conectado'))
-.catch(err => console.error('Error de conexión:', err));
+  useUnifiedTopology: true
+}).then(() => console.log('Conectado a MongoDB'))
+  .catch(err => console.error('Error de conexión:', err));
+
+app.use('/api', rutas);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
